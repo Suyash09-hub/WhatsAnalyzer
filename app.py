78bae88b1faff25190d563f57c2ce20272a774a9
Suyash Srivastava <Suyash.sri.0902@gmail.com>
2023-05-03 00:54:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ if uploaded_file is not None:
 
     # fetch unique users
     user_list = df['user'].unique().tolist()
-    user_list.remove('group_notification')
     user_list.sort()
     user_list.insert(0,"Overall")
 
