beba47b6890ba7854a1c0e90addfdd7b112f0dcb
Suyash Srivastava <Suyash.sri.0902@gmail.com>
2023-05-04 03:08:25 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -10,7 +10,7 @@ def preprocess(data):
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
     # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%m/%d/%y, %H:%M %p -')
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M %p -')
 
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
