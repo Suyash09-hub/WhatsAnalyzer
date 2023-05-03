90a4411afa921fcf58b156cee01d5fd48b6e566f
Suyash Srivastava <Suyash.sri.0902@gmail.com>
2023-05-04 02:42:00 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -2,7 +2,8 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = '^\d{2}\/\d{2}\/\d{2},\s\d{1,2}:\d{2}\s[ap]m\s-\s[a-zA-Z]+\s[a-zA-Z]+:\s\d$'
+
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
