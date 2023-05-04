cb8fa1e00eb2e1cf414e2f756750dc7cd41f845d
Suyash Srivastava <Suyash.sri.0902@gmail.com>
2023-05-04 22:00:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,26 +113,4 @@ if uploaded_file is not None:
         st.title('Most commmon words')
         st.pyplot(fig)
 
-        # emoji analysis
-        emoji_df = helper.emoji_helper(selected_user,df)
-        st.title("Emoji Analysis")
-
-        col1,col2 = st.columns(2)
-
-        with col1:
-            st.dataframe(emoji_df)
-        with col2:
-            fig,ax = plt.subplots()
-            ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct="%0.2f")
-            st.pyplot(fig)
-
-
-
-
-
-
-
-
-
-
-
+        
